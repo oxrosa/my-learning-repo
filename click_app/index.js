d0467663d3fddfba43e57476cdf49f7c484b516f
Rohit <allneedjava@gmail.com>
2019-09-02 15:18:21 +0530

    changes from oxrosa

--- click_app/index.js
+++ click_app/index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 //Hello
-//World
+//These changes are from 
 AppRegistry.registerComponent(appName, () => App);
